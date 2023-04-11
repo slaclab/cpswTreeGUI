85f7c9c7bc9bb3e8ef0603f70f9b70d0134bbcda
jbellister-slac <89539359+jbellister-slac@users.noreply.github.com>
2023-04-11 10:54:08 -0700

    Update env.slac.sh to point to R4.5.1 of cpsw which includes the change to use a conda environment in the build process

--- env.slac.sh
+++ env.slac.sh
@@ -1,3 +1,3 @@
-. /afs/slac/g/lcls/package/cpsw/framework/R4.5.0/rhel6-x86_64/bin/env-cpsw.sh
+. /afs/slac/g/lcls/package/cpsw/framework/R4.5.1/rhel6-x86_64/bin/env-cpsw.sh
 . /afs/slac/g/lcls/package/anaconda/envs/python3.8envs/v2.5/bin/activate
 export PYTHONPATH=${PYTHONPATH}:$(dirname -- "$(readlink -f $0)")
