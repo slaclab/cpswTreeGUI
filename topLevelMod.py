e16c04efa70bc4b5ce3f69b0af61b68dd7730702
Michael Skoufis <skoufis@stanford.edu>
2024-10-25 13:01:02 -0700

    Add logic to replace alphanumeric ports with 8192

--- topLevelMod.py
+++ topLevelMod.py
@@ -14,7 +14,7 @@ import subprocess
 import numpy as np
 
 # Global vars
-remove_stream_blocks   = ['strm','stream','Stream']
+remove_stream_blocks   = ['strm','stream','Stream','Bsa','bsa','BSA']
 remove_mmio_blocks     = ['rssi','RSSI:','depack:','Depack:','TDEST:','TDESTMux:']
 replace_srp_protocol   = ['SRP_UDP_V3']
 typical_udp_ports      = ['8193','8194','8197','8198']
@@ -48,11 +48,11 @@ def modify_top_level( toplevel_path ):
         # Check if we should keep, modify or remove this line
         if 'mmio:' in line:
           # Beginning of mmio block
-          mmio = True; stream = False
+          mmio = True; mmio_remove = False; stream = False
           modified_lines.append( line )
         elif any([x in line for x in remove_stream_blocks]):
           # Beginning of stream block
-          stream = True; mmio = False
+          stream = True; mmio = False; mmio_remove = False
           stream_indentation = len( line ) - len( line.lstrip() )
         elif mmio:
           # Check if we need to replace the SRP protocol version
@@ -61,15 +61,23 @@ def modify_top_level( toplevel_path ):
             modified_lines.append( nline )
           elif 'port: ' in line:
             # Replace port with a different port number to avoid conflict with the IOC
-            nline = line
-            if len([x for x in typical_udp_ports if x in line]):
-              port = [x for x in typical_udp_ports if x in line][0]
-              nline = line.replace(port,'8192')
+            sline = line.split()
+            if len(sline) > 1:
+              if len([x for x in typical_udp_ports if x in sline[1]]):
+                port = [x for x in typical_udp_ports if x in sline[1]][0]
+                nline = line.replace(port,'8192')
+              elif not sline[1].isnumeric():
+                nline = line.replace(sline[1],'8192')
+              else:
+                nline = line
             modified_lines.append( nline )
           # Remove mmio properties that pertain to RSSI, depack and TDESTMux
           elif any([x in line for x in remove_mmio_blocks]):
+            mmio_remove = True 
+          elif mmio_remove:
             pass
           else:
+            mmio_remove = False
             modified_lines.append( line )
         # Remove all lines in a stream block
         elif stream:
